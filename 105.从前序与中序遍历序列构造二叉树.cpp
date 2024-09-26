6166255e6e65a9557b481ad7863b57bba9cad5d4
Lukehututu <a1443392845@gmail.com>
2024-09-26 09:26:56 +0800

    9_26

---
+++ 105.从前序与中序遍历序列构造二叉树.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=105 lang=cpp
+ *
+ * [105] 从前序与中序遍历序列构造二叉树
+ *
+ * https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (71.98%)
+ * Likes:    2382
+ * Dislikes: 0
+ * Total Accepted:    714.1K
+ * Total Submissions: 991.2K
+ * Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+ *
+ * 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder
+ * 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+ * 输出: [3,9,20,null,null,15,7]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: preorder = [-1], inorder = [-1]
+ * 输出: [-1]
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 <= preorder.length <= 3000
+ * inorder.length == preorder.length
+ * -3000 <= preorder[i], inorder[i] <= 3000
+ * preorder 和 inorder 均 无重复 元素
+ * inorder 均出现在 preorder
+ * preorder 保证 为二叉树的前序遍历序列
+ * inorder 保证 为二叉树的中序遍历序列
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+
+        int size = preorder.size();
+        return buildTree(preorder,0,size - 1,inorder,0,size-1);
+    }
+ 
+    TreeNode* buildTree(vector<int>& preorder,int i,int j,vector<int>& inorder,int m,int n)
+    {
+        if(i > j || m > n)
+            return nullptr;
+
+        TreeNode* head = new TreeNode(preorder[i]);
+        for(int k = m; k <= n;k++)
+        {
+            if(inorder[k] == preorder[i])
+            {
+                head->left = buildTree(preorder, i + 1, i + k - m, inorder, m , k - 1);
+                head->right = buildTree(preorder, i + k - m + 1, j, inorder, k + 1, n);
+                break;
+            }
+        }
+        return head;
+    }
+};
+// @lc code=end
+
