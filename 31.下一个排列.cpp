79c7a2d15205e445864388cf7708b990499e6d06
Lukehututu <a1443392845@gmail.com>
2024-11-21 00:11:38 +0800

    21-2024-11

---
+++ 31.下一个排列.cpp
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=cpp
+ *
+ * [31] 下一个排列
+ *
+ * https://leetcode.cn/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (39.79%)
+ * Likes:    2581
+ * Dislikes: 0
+ * Total Accepted:    559.5K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 整数数组的一个 排列  就是将其所有成员以序列或线性顺序排列。
+ * 
+ * 
+ * 例如，arr = [1,2,3] ，以下这些都可以视作 arr 的排列：[1,2,3]、[1,3,2]、[3,1,2]、[2,3,1] 。
+ * 
+ * 
+ * 整数数组的 下一个排列 是指其整数的下一个字典序更大的排列。更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，那么数组的 下一个排列
+ * 就是在这个有序容器中排在它后面的那个排列。如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（即，其元素按升序排列）。
+ * 
+ * 
+ * 例如，arr = [1,2,3] 的下一个排列是 [1,3,2] 。
+ * 类似地，arr = [2,3,1] 的下一个排列是 [3,1,2] 。
+ * 而 arr = [3,2,1] 的下一个排列是 [1,2,3] ，因为 [3,2,1] 不存在一个字典序更大的排列。
+ * 
+ * 
+ * 给你一个整数数组 nums ，找出 nums 的下一个排列。
+ * 
+ * 必须 原地 修改，只允许使用额外常数空间。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [1,2,3]
+ * 输出：[1,3,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [3,2,1]
+ * 输出：[1,2,3]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [1,1,5]
+ * 输出：[1,5,1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        bool isDesc = false;
+        //1.找第一对a[i] < a[i + 1]
+        int i{};
+        for (i = n - 2; i >= 0; --i) {
+            if (nums[i] < nums[i + 1])
+                break;
+        }
+        if (i == -1) //说明本身就是降序
+            isDesc = true;
+        int j{};
+        if (!isDesc) {
+            //2.找第一对a[i] < a[j] ->swap(i,j);
+            for (j = n - 1; j > i; --j) {
+                if (nums[j] > nums[i])
+                    break;
+            }
+            swap(nums[i], nums[j]);
+        }
+        //交换完，i+1 ~ n-1依旧是降序排列
+        //3.升序排列a[i + 1],a[n]
+        for (j = n - 1,i += 1; j >= i; --j, ++i) {
+            swap(nums[i], nums[j]);
+        }
+    }
+};
+// @lc code=end
+
